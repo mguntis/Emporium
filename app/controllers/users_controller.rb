8c69f8ca40da17bd554ca03c01d4977d84f1fdb6
mguntis <m.guntis@gmail.com>
2017-03-22 01:56:40 +0200

    ErrorFix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+
     if @user.save
-      flash[:notice] = "You Signed up successfully"
-      flash[:color]= "valid"
+       redirect_to 'new', notice: 'Post was successfully updated.'
     else
       flash[:notice] = "Form is invalid"
       flash[:color]= "invalid"
     end
-    render "new"
-  end
+    # render "new"
+    end
 
 end
